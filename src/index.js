ccff6bb04ddb7e51c8c3d277009bee9b20d9c0f2
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-11-27 13:43:20 -0600

    feat: add morgan middleware for logging requests

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 
 import { EXPRESS_PORT } from './config.js'
 import appRouter from './routes/contacto.routes.js'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(appRouter)
 
